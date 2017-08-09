cb476d5feb717c75862fc66c29629eae007aba4d
shizuna.ito <shizuna.ito@pepabo.com>
2017-08-09 19:03:48 +0900

    [WIP]イメージ切り取り

--- ImageClipper/ViewController.swift
+++ ImageClipper/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clippedImageView: UIImageView!
+    
+    let image = UIImage(named: "flowers.png")
+
+    // マスク用画像
+    let maskImage = UIImage(named:"ninjya.png")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        clippedImageView.image = image
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func choiceImageButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clipButtonClicked(_ sender: UIButton) {
+        guard let rootImage = image else {
+            return
+        }
+        clippedImageView.image = rootImage.mask(image: maskImage)
 
+    }
+}
+
+extension UIImage {
+    func mask(image: UIImage?) -> UIImage {
+        if let maskRef = image?.cgImage,
+            let ref = cgImage,
+            let mask = CGImage(maskWidth: maskRef.width,
+                               height: maskRef.height,
+                               bitsPerComponent: maskRef.bitsPerComponent,
+                               bitsPerPixel: maskRef.bitsPerPixel,
+                               bytesPerRow: maskRef.bytesPerRow,
+                               provider: maskRef.dataProvider!,
+                               decode: nil,
+                               shouldInterpolate: false),
+            let output = ref.masking(mask) {
+            return UIImage(cgImage: output)
+        }
+        return self
+    }
 }
 
