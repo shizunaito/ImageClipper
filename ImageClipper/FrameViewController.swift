4fa703765ca98d921fbb5d5c6a4dc91eda1a8338
Shizuna Ito <adfa1728@chiba-u.jp>
2017-08-20 22:44:44 +0900

    Add FrameViewController

---
+++ ImageClipper/FrameViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class FrameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ 
+    }
+
+}
