ed21bf681e31cd0530284cab7c29e4065d1d694e
Shizuna Ito <adfa1728@chiba-u.jp>
2017-12-25 22:02:40 +0900

    UIImageのextensionを切り出した

---
+++ ImageClipper/UIImage+mask.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UIKit
+
+extension UIImage {
+    func mask(image: UIImage?) -> UIImage {
+        if let maskRef = image?.cgImage,
+            let ref = cgImage,
+            let mask = CGImage(maskWidth: maskRef.width,
+                               height: maskRef.height,
+                               bitsPerComponent: maskRef.bitsPerComponent,
+                               bitsPerPixel: maskRef.bitsPerPixel,
+                               bytesPerRow: maskRef.bytesPerRow,
+                               provider: maskRef.dataProvider!,
+                               decode: nil,
+                               shouldInterpolate: false),
+            let output = ref.masking(mask) {
+            return UIImage(cgImage: output)
+        }
+        return self
+    }
+}
--- ImageClipper/ViewController.swift
+++ ImageClipper/ViewController.swift
@@ -53,22 +53,3 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
-extension UIImage {
-    func mask(image: UIImage?) -> UIImage {
-        if let maskRef = image?.cgImage,
-            let ref = cgImage,
-            let mask = CGImage(maskWidth: maskRef.width,
-                               height: maskRef.height,
-                               bitsPerComponent: maskRef.bitsPerComponent,
-                               bitsPerPixel: maskRef.bitsPerPixel,
-                               bytesPerRow: maskRef.bytesPerRow,
-                               provider: maskRef.dataProvider!,
-                               decode: nil,
-                               shouldInterpolate: false),
-            let output = ref.masking(mask) {
-            return UIImage(cgImage: output)
-        }
-        return self
-    }
-}
-
